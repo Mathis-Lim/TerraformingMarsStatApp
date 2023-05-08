4e94bdb986a21706fc1a101c788f4cb4921f40f1
Mathis Lim <mathis.lim@etu.umontpellier.fr>
2023-05-08 16:26:10 +0200

    [FIX]Connexion

--- App/Model/ConnectionModel.php
+++ App/Model/ConnectionModel.php
@@ -5,14 +5,14 @@
 		private static $pdo = NULL;
 
 		public static function init(){
-			$hostname = "127.0.0.1";
+			$hostname = "mysql:unix_socket=/run/mysqld/mysqld.sock";
 			$port = "3306";
 			$database_name = "TerraformingMars";
 			$login = "root";
 			$password = "Root1234";
 
 			try {
-  				self::$pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database_name", $login, $password,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+  				self::$pdo = new PDO("mysql:host=$hostname;dbname=$database_name", $login, $password,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 			} catch (PDOException $e) {
     				echo 'Une erreur est survenue';
   				die();
