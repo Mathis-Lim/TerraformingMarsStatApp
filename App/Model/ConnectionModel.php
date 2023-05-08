bbdd65e873785aca0b8f978268368c270242f5b3
Mathis Lim <mathis.lim@etu.umontpellier.fr>
2023-05-08 15:51:37 +0200

    [FIX]Connexion BD

--- App/Model/ConnectionModel.php
+++ App/Model/ConnectionModel.php
@@ -13,11 +13,7 @@
 			try {
   				self::$pdo = new PDO("mysql:host=$hostname;dbname=$database_name", $login, $password,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 			} catch (PDOException $e) {
-  				if (Conf::getDebug()) {
-    				echo $e->getMessage(); // affiche un message d'erreur
-  				} else {
     				echo 'Une erreur est survenue';
-  				}
   				die();
 			}
 
