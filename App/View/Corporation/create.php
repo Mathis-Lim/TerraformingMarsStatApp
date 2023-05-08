eb30c9cdb7f42e90df003911f6bd2eef087baecb
Mathis Lim <mathis.lim@etu.umontpellier.fr>
2023-05-08 15:48:24 +0200

    [FIX]Créer une corporation

--- App/View/Corporation/create.php
+++ App/View/Corporation/create.php
@@ -3,6 +3,7 @@
   <fieldset>
     <legend>Enregistrer une corporation:</legend>
     <input type='hidden' name='action' value='created'>
+    <input type='hidden' name='controller' value='corporation'>
     <p>
         <label for = "corporation_name">Nom </label>:
         <input type = "text" name = "name" id = "corporation_name" required/>
