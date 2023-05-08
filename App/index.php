891d7bb11740e48b4b54770626a90dd3f6ea6a8f
Mathis Lim <mathis.lim@etu.umontpellier.fr>
2023-05-08 12:40:42 +0200

    [FIX]MVC

--- App/index.php
+++ App/index.php
@@ -1,5 +1,5 @@
 <?php
 	$DS = DIRECTORY_SEPARATOR;
-	require __DIR__.$DS."lib".$DS."File.php";
+	require __DIR__.$DS."Core".$DS."File.php";
 	require File::build_path(array('Controller','routeur.php'));
 ?>
