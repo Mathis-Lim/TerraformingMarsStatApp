27783df151c03d9e72b3805ac8d92110abeecfbd
Mathis Lim <mathis.lim@etu.umontpellier.fr>
2023-05-08 18:13:45 +0200

    [FIX]Gestion d'erreur

--- App/Controller/ErrorController.php
+++ App/Controller/ErrorController.php
@@ -2,9 +2,18 @@
 
     Class ErrorController{
 
-        public function url(){
-            $controller = "error";
+        public static function url(){
             $errorMessage = "Une erreur est survenue lors de la navigation vers vautre page";
+            require File::build_path(array("View", "ErrorView.php"));
+        }
+
+        public static function corporationCreation(){
+            $errorMessage = "Une erreur est survenue lors de la création de la corporation";
+            require File::build_path(array("View", "ErrorView.php"));
+        }
+
+        public static function readAllCorporation(){
+            $errorMessage = "Une erreur est survenue lors de l'affichage des corporations";
             require File::build_path(array("View", "ErrorView.php"));
         }
 
