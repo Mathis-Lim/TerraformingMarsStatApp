3a511d07530fce5753f8ebdeede29ea3ba6f2ba8
Mathis Lim <mathis.lim@etu.umontpellier.fr>
2023-05-09 19:22:51 +0200

    [WIP]GameDetailModel

---
+++ App/Model/GameDetailModel.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once File::build_path(array('Model','ConnectionModel.php'));
+
+    class GameDetailModel{
+
+        private $gameId;
+	    private $playerId;
+	    private $chosenCorporation;
+	    private $rejectedCorporation;
+	    private $rank;
+	    private $trScore;
+	    private $boardScore;
+	    private $cardScore;
+	    private $goalScore;
+	    private $awardScore;
+	    private $score;
+
+    }
