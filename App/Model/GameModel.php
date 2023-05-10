7846ecb0a0346f074e36d6b0eac7e9e9de24a5f8
Mathis Lim <mathis.lim@etu.umontpellier.fr>
2023-05-10 20:25:34 +0200

    [FIX]Récupération dernière partie

--- App/Model/GameModel.php
+++ App/Model/GameModel.php
@@ -71,7 +71,7 @@
             $res = ConnectionModel::getPDO()->query($sql);
             $res->setFetchMode(PDO::FETCH_OBJ);
             $result = $res->fetchAll();
-            $nb = $result[0]->{'MAX(gameId'};
+            $nb = $result[0]->{'MAX(gameId)'};
             return $id;
         }
 
