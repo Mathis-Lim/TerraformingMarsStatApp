c95d324fb18f7f0b2a1e8a7bac539396c0c2e596
Mathis Lim <mathis.lim@etu.umontpellier.fr>
2023-06-22 11:34:42 +0200

    [FIX]Supression code inutile

--- App/Model/PlayerModel.php
+++ App/Model/PlayerModel.php
@@ -44,14 +44,6 @@
         }
 
         public static function getPlayerById($id){
-			/*$sql = "SELECT * FROM Players WHERE playerId=:player_id";
-			$req_prep = ConnectionModel::getPDO()->prepare($sql);
-			$values = array("player_id" => $id,);
-			$req_prep->execute($values);
-			$req_prep->setFetchMode(PDO::FETCH_CLASS, 'PlayerModel');
-			$res = $req_prep->fetchAll();
-			$player = $res[0];
-			return $player;*/
 			return GenericModel::getById('Player', $id);
         }
 
