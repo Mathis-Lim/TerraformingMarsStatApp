9430b17bae254c36fa802bd3970a8e112b6805ad
Mathis Lim <mathis.lim@etu.umontpellier.fr>
2023-06-22 15:06:55 +0200

    [FIX]Grammaire

--- App/View/Player/detail.php
+++ App/View/Player/detail.php
@@ -74,7 +74,7 @@
 <?php    
     echo('<tr><th>Nom</th>');
     echo('<th>Fréquence de choix</th>');
-    echo('<th>Distribuée(s)</th></tr>');
+    echo('<th>Distribuée</th></tr>');
     foreach($corporationChoices as $corporation){
         echo('<tr><td>' . $corporation['name'] . '</td>');
         echo('<td>' . round($corporation['frequency'] * 100, 2) . '%</td>');
