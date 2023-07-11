bb9042f81574549c2c054afb8a9f0c54f027c258
Mathis Lim <mathis.lim@etu.umontpellier.fr>
2023-07-11 17:48:45 +0200

    [FIX]Syntaxe

--- App/View/Game/home.php
+++ App/View/Game/home.php
@@ -19,7 +19,7 @@
 ?>
 </ul>
 <?php
-    echo('<a href = "index.php?controller=GameDetail&action=readGame&game_id=' . $lastGameId . '>Détail</a>');
+    echo('<a href = "index.php?controller=GameDetail&action=readGame&game_id=' . $lastGameId . "'>Détail</a>');
 
 
 
